158dc1d25bd1a39de1d61fe9572faf3dc3ae4a27
Jason van Brecht <jason-github@vanbrecht.com>
2021-01-08 02:34:26 -0500

    main

--- Grbl_Esp32/src/Machine.h
+++ Grbl_Esp32/src/Machine.h
@@ -8,7 +8,7 @@
 // !!! For initial testing, start with test_drive.h which disables
 // all I/O pins
 // #include "Machines/atari_1020.h"
-#    include "Machines/lead_1010_openbuilds_2130.h"
+#    include "Machines/lead_1010_openbuilds_external.h"
 
 // !!! For actual use, change the line above to select a board
 // from Machines/, for example:
--- Grbl_Esp32/src/Machines/lead_1010_openbuilds_external.h
+++ Grbl_Esp32/src/Machines/lead_1010_openbuilds_external.h
@@ -31,7 +31,7 @@
     along with Grbl_ESP32.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define MACHINE_NAME "LEAD_1010_OPENBUILDS_XYZ_2130"
+#define MACHINE_NAME "LEAD_1010_OPENBUILDS_EXTERNAL"
 
 
 #define N_AXIS 3
@@ -39,37 +39,26 @@
 #define USE_I2S_OUT
 #define USE_I2S_STEPS
 
-#define TRINAMIC_RUN_MODE           TrinamicMode::StealthChop
-#define TRINAMIC_HOMING_MODE        TrinamicMode::StealthChop
 
-
-#define X_TRINAMIC_DRIVER       2130
 #define X_DISABLE_PIN           I2SO(0)
 #define X_DIRECTION_PIN         I2SO(1)
 #define X_STEP_PIN              I2SO(2)
 #define X_CS_PIN                I2SO(3)
-#define X_RSENSE                TMC2130_RSENSE_DEFAULT
 
-#define Y_TRINAMIC_DRIVER       2130
 #define Y_DIRECTION_PIN         I2SO(4)
 #define Y_STEP_PIN              I2SO(5)
 #define Y_DISABLE_PIN           I2SO(7)
 #define Y_CS_PIN                I2SO(6)
-#define Y_RSENSE                X_RSENSE
 
-#define Y2_TRINAMIC_DRIVER       2130
 #define Y2_DISABLE_PIN           I2SO(8)
 #define Y2_DIRECTION_PIN         I2SO(9)
 #define Y2_STEP_PIN              I2SO(10)
 #define Y2_CS_PIN                I2SO(11)
-#define Y2_RSENSE                X_RSENSE
 
-#define Z_TRINAMIC_DRIVER       2130
 #define Z_DIRECTION_PIN         I2SO(12)
 #define Z_STEP_PIN              I2SO(13)
 #define Z_DISABLE_PIN           I2SO(15)
 #define Z_CS_PIN                I2SO(14)
-#define Z_RSENSE                X_RSENSE
 
 
 /* Spindle Control Use Module slot 3*/
